2877f05b71f8f964d4cda38fee1d2c784b5a1dd8
Rocco <53787882+Ultitomate@users.noreply.github.com>
2022-08-01 17:54:58 +0200

    0.35
    added stuff

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var FRICTION = 500
+export var ROLL_SPEED = 120
 
 enum {
 	MOVE,
@@ -12,13 +13,16 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $"HitboxPivot/Sword-Hitbox"
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 func _physics_process(delta):
 	match state:
@@ -26,7 +30,7 @@ func _physics_process(delta):
 			move_state(delta)
 			
 		ROLL:
-			pass
+			roll_state(delta)
 			
 		ATTACK:
 			attack_state(delta)
@@ -46,23 +50,43 @@ func move_state(delta):
 	
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
+	move()
 	
-	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("Roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 	
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
 func attack_state(delta):
 	animationState.travel("Attack")
 	
+func move():
+	velocity = move_and_slide(velocity)
+
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
+
 func attack_animation_finished():
 	state = MOVE
+	
+
---
+++ szenes/Enemies/Bat.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var knockback = Vector2.ZERO
+
+onready var stats = $Stats
+
+
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, 200* delta)
+	knockback = move_and_slide(knockback)
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	print(stats.health)
+	knockback = area.knockback_vector * 130
