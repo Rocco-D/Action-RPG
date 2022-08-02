31ff3d375b14a7a13aa1ef692e27ebc93ff2ec6c
Rocco <53787882+Ultitomate@users.noreply.github.com>
2022-08-02 18:23:56 +0200

    0.36
    stuff

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,7 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.LEFT
+var roll_vector = Vector2.DOWN
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -44,7 +44,7 @@ func _physics_process(delta):
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_raw_strength("ui_left")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_raw_strength("westen")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
--- szenes/Enemies/Bat.gd
+++ szenes/Enemies/Bat.gd
@@ -1,15 +1,23 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://szenes/Effekts/EnemyDeathEffect.tscn")
+
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
-
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200* delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	stats.health -= 1
-	print(stats.health)
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 130
+
+
+func _on_Stats_no_health():
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
+	
