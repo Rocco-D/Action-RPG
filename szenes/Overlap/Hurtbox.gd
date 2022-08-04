181f8e0a15a404371cf9f2eb464eca8695ce8345
Rocco <53787882+Ultitomate@users.noreply.github.com>
2022-08-04 20:53:28 +0200

    0.38
    some bat ai

--- szenes/Overlap/Hurtbox.gd
+++ szenes/Overlap/Hurtbox.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
+export(bool) var show_hit = true
+
 const HitEffect = preload("res://Effects/HitEffect.png")
 
 
 func _on_Hurtbox_area_entered(area):
-	var effect = HitEffect.instance() 
-	var main = get_tree().current_scene
-	main.add_child(effect)
-	effect.gloabal_position = global_position
+	if show_hit:
+		#var effect = HitEffect.instance()
+		#var main = get_tree().current_scene
+		#main.add_child(effect)
+		#effect.global_position = global_position
+		pass
+	pass
+	
